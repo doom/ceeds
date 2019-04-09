05b7112078b3627a47f94dc06554c36ed46e7ca1
Doom <clement.doumergue@epitech.eu>
2019-04-09 17:33:54 -0400

    Add a few basic includes and macros to core.h

--- include/ceeds/core.h
+++ include/ceeds/core.h
@@ -5,4 +5,59 @@
 #ifndef CEEDS_CORE_H
 #define CEEDS_CORE_H
 
+#include <assert.h>
+#include <limits.h>
+#include <stdalign.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define likely(x)                   (__builtin_expect(!!(x), 1))
+#define unlikely(x)                 (__builtin_expect((x), 0))
+
+#define _always_inline_             __attribute__((always_inline)) inline
+#define _unused_                    __attribute__((unused))
+#define _pure_                      __attribute__((pure))
+#define _noreturn_                  __attribute__((noreturn))
+#define _aligned_(x)                __attribute__((aligned(x)))
+#define _cleanup_(f)                __attribute__((cleanup(f)))
+#define _constructor_               __attribute__((constructor))
+#define _destructor_                __attribute__((destructor))
+#define _format_printf_(ifmt, iarg) __attribute__((format(printf, ifmt, iarg)))
+
+#define MIN(a, b)                                                           \
+    ({                                                                      \
+        typeof(a) __a = (a);                                                \
+        typeof(b) __b = (b);                                                \
+                                                                            \
+        __a < __b ? __a : __b;                                              \
+    })
+
+#define MAX(a, b)                                                           \
+    ({                                                                      \
+        typeof(a) __a = (a);                                                \
+        typeof(b) __b = (b);                                                \
+                                                                            \
+        __a < __b ? __b : __a;                                              \
+    })
+
+#define CMP(a, b)                                                           \
+    ({                                                                      \
+        typeof(a) __a = (a);                                                \
+        typeof(b) __b = (b);                                                \
+                                                                            \
+        (__b < __a) - (__a < __b);                                          \
+    })
+
+#define SWAP(a, b)                                                          \
+    ({                                                                      \
+        typeof(*(a)) __c = *(a);                                            \
+                                                                            \
+        *(a) = *(b);                                                        \
+        *(b) = __c;                                                         \
+    })
+
+#define array_length(arr)           (sizeof(arr) / sizeof((arr)[0]))
+
 #endif /* !CEEDS_CORE_H */
--- tests/core-tests.c
+++ tests/core-tests.c
@@ -3,5 +3,84 @@
 */
 
 #include "unit_tests.h"
+#include <ceeds/core.h>
 
-ut_group(core);
+ut_test(expectations)
+{
+    ut_assert(likely(true));
+    ut_assert_false(likely(false));
+    ut_assert(unlikely(true));
+    ut_assert_false(unlikely(false));
+}
+
+static int counter = 0;
+
+static int counting_id(int i)
+{
+    ++counter;
+    return i;
+}
+
+ut_test(max)
+{
+    ut_assert_eq(MAX(1, 2), 2);
+    ut_assert_eq(MAX(2, 2), 2);
+
+    counter = 0;
+    ut_assert_eq(MAX(counting_id(1), 2), 2);
+    ut_assert_eq(counter, 1);
+
+    counter = 0;
+    ut_assert_eq(MAX(2, counting_id(1)), 2);
+    ut_assert_eq(counter, 1);
+}
+
+ut_test(min)
+{
+    ut_assert_eq(MIN(1, 2), 1);
+    ut_assert_eq(MIN(2, 2), 2);
+
+    counter = 0;
+    ut_assert_eq(MIN(counting_id(1), 0), 0);
+    ut_assert_eq(counter, 1);
+
+    counter = 0;
+    ut_assert_eq(MIN(0, counting_id(1)), 0);
+    ut_assert_eq(counter, 1);
+}
+
+ut_test(cmp)
+{
+    ut_assert_eq(CMP(1, 2), -1);
+    ut_assert_eq(CMP(2, 1), 1);
+    ut_assert_eq(CMP(1, 1), 0);
+    ut_assert_eq(CMP(INT_MIN, 1), -1);
+    ut_assert_eq(CMP(1, INT_MIN), 1);
+    ut_assert_eq(CMP(INT_MIN, INT_MIN), 0);
+}
+
+ut_test(swap)
+{
+    int a = 0;
+    int b = 1;
+
+    SWAP(&a, &b);
+    ut_assert_eq(a, 1);
+    ut_assert_eq(b, 0);
+}
+
+ut_test(array_length)
+{
+    int arr[6];
+
+    ut_assert_eq(array_length(arr), 6);
+}
+
+ut_group(core,
+         ut_get_test(expectations),
+         ut_get_test(max),
+         ut_get_test(min),
+         ut_get_test(cmp),
+         ut_get_test(swap),
+         ut_get_test(array_length)
+);
